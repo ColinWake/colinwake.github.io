9b2cb7715ae8cd8c51cc873ad47b7edf69aa94ea
Colin Wakefield <colinpwakefield@gmail.com>
2020-02-04 20:56:31 -0500

    Reformatting

--- tab.js
+++ tab.js
@@ -1,12 +1,12 @@
-        function tab(page, clickedTab) {
-            var a = document.getElementsByClassName("tab")
-            for (var i = 0; i < a.length; i++) {
-                a[i].classList.add('hidden');
-            }
-            document.getElementById(page).classList.remove('hidden');
-            var a = document.getElementsByClassName("navbar__active")
-            for (var i = 0; i < a.length; i++) {
-                a[i].className = "navbar__nav";
-            }
-            clickedTab.className = "navbar__active";
-        }
+function tab(page, clickedTab) {
+    var a = document.getElementsByClassName("tab")
+    for (var i = 0; i < a.length; i++) {
+        a[i].classList.add('hidden');
+    }
+    document.getElementById(page).classList.remove('hidden');
+    var a = document.getElementsByClassName("navbar__active")
+    for (var i = 0; i < a.length; i++) {
+        a[i].className = "navbar__nav";
+    }
+    clickedTab.className = "navbar__active";
+}
